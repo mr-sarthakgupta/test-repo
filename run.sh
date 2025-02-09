9a4f7e51945ee4bc6f8197618dda50f3bc4d22e6
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2025-02-10 01:53:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # We have 8 GPUs available, but we need only 4 for the current batch sizes.
-batch_size=(1000 500 250 20)
+batch_size=(1000 500 250 20 500)
 
-for i in {0..3}; do
+for i in {0..4}; do
   CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
 done
 
